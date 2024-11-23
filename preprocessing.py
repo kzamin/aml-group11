8b94065d1a1ec329d63b6946de49d1f34e0a815c
Arnav Mahajan <arnav.mahajan03@gmail.com>
2024-11-22 18:42:52 -0800

    Add severity scores calculations, remove duplicates

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
+def encode_class_scores(violation_df: pd.DataFrame):
+    violation_df['class'] = violation_df['class'].map(lambda x: ord(x) - ord('A'))
+    return violation_df
+
 def calculate_append_severity_scores(violation_df: pd.DataFrame):
-    pass
+    violation_df['risk_score'] = violation_df.groupby(['violationid'])['class'].transform('sum')
+    violation_df['risk_score_cat'] = violation_df['risk_score'].clip(upper=4)
+    violation_df = violation_df.drop_duplicates(subset=['violationid'])
+    return violation_df
